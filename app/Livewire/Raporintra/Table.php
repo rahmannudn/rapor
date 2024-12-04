cf0c36f7a6f09916280378952fc2970798b83f70
rahmanuddin <rahmannudn0@gmail.com>
2024-12-04 21:38:55 +0800

    menambahkan filter tahun ajaran pada rapor intra

--- app/Livewire/Raporintra/Table.php
+++ app/Livewire/Raporintra/Table.php
@@ -2,6 +2,7 @@
 
 namespace App\Livewire\Raporintra;
 
+use App\Helpers\FunctionHelper;
 use Livewire\Component;
 use App\Models\WaliKelas;
 use Illuminate\Support\Facades\Cache;
@@ -11,17 +12,26 @@ class Table extends Component
     public $searchQuery;
     public $show = 10;
 
+    public $selectedTahunAjaran;
+    public $daftarTahunAjaran;
+
+    public function mount()
+    {
+        $this->selectedTahunAjaran = Cache::get('tahunAjaranAktif');
+        $this->daftarTahunAjaran = FunctionHelper::getDaftarTahunAjaranByWaliKelas();
+    }
+
     public function render()
     {
-        $tahunAjaranAktif = Cache::get('tahunAjaranAktif');
         $dataSiswa = WaliKelas::search($this->searchQuery)
             ->where('user_id', auth()->id())
-            ->where('wali_kelas.tahun_ajaran_id', $tahunAjaranAktif)
+            ->where('wali_kelas.tahun_ajaran_id', $this->selectedTahunAjaran)
             ->join('kelas_siswa', 'kelas_siswa.kelas_id', 'wali_kelas.kelas_id')
-            ->where('kelas_siswa.tahun_ajaran_id', $tahunAjaranAktif)
+            ->where('kelas_siswa.tahun_ajaran_id', $this->selectedTahunAjaran)
+            ->join('kelas', 'kelas.id', 'kelas_siswa.kelas_id')
             ->join('siswa', 'siswa.id', 'kelas_siswa.siswa_id')
             ->orderBy('siswa.nama', 'ASC')
-            ->select('siswa.id', 'siswa.nama', 'kelas_siswa.id as kelas_siswa_id')
+            ->select('siswa.id', 'siswa.nama', 'kelas_siswa.id as kelas_siswa_id', 'kelas.nama as nama_kelas')
             ->paginate($this->show);
 
         return view('livewire.raporintra.table', compact('dataSiswa'));
--- resources/views/livewire/raporintra/table.blade.php
+++ resources/views/livewire/raporintra/table.blade.php
@@ -1,14 +1,24 @@
 <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
     {{-- show & search --}}
     <div class="block mb-4 space-y-2 md:flex md:items-center md:justify-between md:space-y-0 md:space-x-2">
-        <div class="block md:w-20">
-            <x-native-select label="Show" wire:model.change='show'>
-                <option value="">Semua</option>
-                <option value="10">10</option>
-                <option value="20">20</option>
-                <option value="50">50</option>
-                <option value="100">100</option>
-            </x-native-select>
+        <div class="flex flex-row items-center space-x-2 md:w-[40%]">
+            <div class="block md:w-20">
+                <x-native-select label="Show" wire:model.change='show'>
+                    <option value="">Semua</option>
+                    <option value="10">10</option>
+                    <option value="20">20</option>
+                    <option value="50">50</option>
+                    <option value="100">100</option>
+                </x-native-select>
+            </div>
+
+            <div class="max-w-52">
+                <x-native-select label="Tahun Ajaran" wire:model.change='selectedTahunAjaran' class="w-[30%]">
+                    @foreach ($daftarTahunAjaran as $TA)
+                        <option value="{{ $TA->id }}">{{ $TA->tahun }} {{ $TA->semester }}</option>
+                    @endforeach
+                </x-native-select>
+            </div>
         </div>
 
         <div class="block md:w-80">
@@ -25,6 +35,9 @@
                 <th scope="col" class="px-6 py-3">
                     Nama Siswa
                 </th>
+                <th scope="col" class="px-6 py-3">
+                    Kelas
+                </th>
                 <th scope="col" class="px-4 py-3">
                     Action
                 </th>
@@ -40,6 +53,10 @@
 
                     <th scope="row" class="px-6 py-3 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                         {{ $siswa->nama }}
+                    </th>
+
+                    <th scope="row" class="px-6 py-3 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                        {{ $siswa->nama_kelas }}
                     </th>
                     <td class="px-4 py-4">
                         <x-button
