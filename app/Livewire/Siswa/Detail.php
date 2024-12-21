ad408239a003310b95e99588ab92fe0b61daaf46
rahmanuddin <rahmannudn0@gmail.com>
2024-12-21 10:00:36 +0800

    memperbaiki queryy detail siswa

--- app/Livewire/Siswa/Detail.php
+++ app/Livewire/Siswa/Detail.php
@@ -7,6 +7,8 @@ use App\Models\KelasSiswa;
 use App\Models\Prestasi;
 use App\Models\Siswa;
 use Illuminate\Database\Query\JoinClause;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Gate;
 use Livewire\Component;
 use Livewire\Attributes\Layout;
 use Livewire\Attributes\Title;
@@ -32,34 +34,69 @@ class Detail extends Component
 
     public function getRiwayatProyek()
     {
-        $proyekData = KelasSiswa::where('kelas_siswa.siswa_id', $this->siswa['id'])
-            ->join('kelas', 'kelas.id', 'kelas_siswa.kelas_id')
-            ->join('wali_kelas', 'wali_kelas.kelas_id', 'kelas.id')
-            ->join('tahun_ajaran', 'tahun_ajaran.id', 'kelas_siswa.tahun_ajaran_id')
-            ->join('users', 'users.id', 'wali_kelas.user_id')
-            ->join('proyek', 'proyek.wali_kelas_id', 'wali_kelas.id')
-            ->leftJoin('catatan_proyek', function (JoinClause $q) {
-                $q->on('catatan_proyek.proyek_id', '=', 'proyek.id')
-                    ->on('catatan_proyek.siswa_id', '=', 'kelas_siswa.siswa_id');
-            })
-            ->select(
-                'kelas_siswa.siswa_id as siswa_id',
-                'kelas_siswa.id as kelas_siswa_id',
-                'kelas.nama as nama_kelas',
-                'users.name as nama_wali_kelas',
-                'tahun_ajaran.id as tahun_ajaran_id',
-                'tahun_ajaran.tahun',
-                'tahun_ajaran.semester',
-                'proyek.id as proyek_id',
-                'proyek.judul_proyek',
-                'proyek.deskripsi as deskripsi_proyek',
-                'catatan_proyek.id as catatan_proyek_id',
-                'catatan_proyek.catatan',
-            )
-            ->orderBy('kelas.nama')
-            ->orderBy('tahun_ajaran.id')
-            ->get()
-            ->toArray();
+        if (Gate::allows('isKepsek')) {
+            $proyekData = KelasSiswa::where('kelas_siswa.siswa_id', $this->siswa['id'])
+                ->join('kelas', 'kelas.id', 'kelas_siswa.kelas_id')
+                ->join('wali_kelas', 'wali_kelas.kelas_id', 'kelas.id')
+                ->join('tahun_ajaran', 'tahun_ajaran.id', 'kelas_siswa.tahun_ajaran_id')
+                ->join('users', 'users.id', 'wali_kelas.user_id')
+                ->join('proyek', 'proyek.wali_kelas_id', 'wali_kelas.id')
+                ->leftJoin('catatan_proyek', function (JoinClause $q) {
+                    $q->on('catatan_proyek.proyek_id', '=', 'proyek.id')
+                        ->on('catatan_proyek.siswa_id', '=', 'kelas_siswa.siswa_id');
+                })
+                ->select(
+                    'kelas_siswa.siswa_id as siswa_id',
+                    'kelas_siswa.id as kelas_siswa_id',
+                    'kelas.nama as nama_kelas',
+                    'users.name as nama_wali_kelas',
+                    'tahun_ajaran.id as tahun_ajaran_id',
+                    'tahun_ajaran.tahun',
+                    'tahun_ajaran.semester',
+                    'proyek.id as proyek_id',
+                    'proyek.judul_proyek',
+                    'proyek.deskripsi as deskripsi_proyek',
+                    'catatan_proyek.id as catatan_proyek_id',
+                    'catatan_proyek.catatan',
+                )
+                ->orderBy('kelas.nama')
+                ->orderBy('tahun_ajaran.id')
+                ->get()
+                ->toArray();
+        }
+        if (Gate::allows('isWaliKelas')) {
+            $proyekData = KelasSiswa::where('kelas_siswa.siswa_id', $this->siswa['id'])
+                ->join('kelas', 'kelas.id', 'kelas_siswa.kelas_id')
+                ->join('wali_kelas', function (JoinClause $q) {
+                    $q->on('wali_kelas.kelas_id', '=', 'kelas.id')
+                        ->where('wali_kelas.user_id', '=', Auth::id());
+                })
+                ->join('tahun_ajaran', 'tahun_ajaran.id', 'kelas_siswa.tahun_ajaran_id')
+                ->join('users', 'users.id', 'wali_kelas.user_id')
+                ->join('proyek', 'proyek.wali_kelas_id', 'wali_kelas.id')
+                ->leftJoin('catatan_proyek', function (JoinClause $q) {
+                    $q->on('catatan_proyek.proyek_id', '=', 'proyek.id')
+                        ->on('catatan_proyek.siswa_id', '=', 'kelas_siswa.siswa_id');
+                })
+                ->select(
+                    'kelas_siswa.siswa_id as siswa_id',
+                    'kelas_siswa.id as kelas_siswa_id',
+                    'kelas.nama as nama_kelas',
+                    'users.name as nama_wali_kelas',
+                    'tahun_ajaran.id as tahun_ajaran_id',
+                    'tahun_ajaran.tahun',
+                    'tahun_ajaran.semester',
+                    'proyek.id as proyek_id',
+                    'proyek.judul_proyek',
+                    'proyek.deskripsi as deskripsi_proyek',
+                    'catatan_proyek.id as catatan_proyek_id',
+                    'catatan_proyek.catatan',
+                )
+                ->orderBy('kelas.nama')
+                ->orderBy('tahun_ajaran.id')
+                ->get()
+                ->toArray();
+        }
 
         return $this->processData($proyekData);
     }
