a21f7b5de6ee33b7a4e9379320d4eee1bf9c5349
rahmanuddin <rahmannudn0@gmail.com>
2024-12-29 15:44:25 +0800

    format dan kirim data hasil query ke template laporan

--- app/Http/Controllers/RiwayatWaliKelasController.php
+++ app/Http/Controllers/RiwayatWaliKelasController.php
@@ -11,6 +11,22 @@ class RiwayatWaliKelasController extends Controller
 {
     public function __invoke()
     {
+        // $data = DB::table('wali_kelas')
+        //     ->join('tahun_ajaran', 'tahun_ajaran.id', 'wali_kelas.tahun_ajaran_id')
+        //     ->join('kelas', function (JoinClause $q) {
+        //         $q->on('kelas.id', '=', 'wali_kelas.kelas_id')
+        //             ->on('kelas.tahun_ajaran_id', '=', 'tahun_ajaran.id');
+        //     })
+        //     ->join('users', 'users.id', 'wali_kelas.user_id')
+        //     ->select(
+        //         'tahun_ajaran.semester',
+        //         'tahun_ajaran.tahun',
+        //         'kelas.nama as nama_kelas',
+        //         'users.name as nama_wali_kelas',
+        //         'tahun_ajaran.id as tahun_ajaran_id'
+        //     )
+        //     ->orderBy('kelas.nama')
+        //     ->get();
         $data = DB::table('kelas')
             ->join('tahun_ajaran', 'tahun_ajaran.id', 'kelas.tahun_ajaran_id')
             ->join('wali_kelas', function (JoinClause $q) {
@@ -19,20 +35,42 @@ class RiwayatWaliKelasController extends Controller
             })
             ->join('users', 'users.id', 'wali_kelas.user_id')
             ->select(
-                'tahun_ajaran.id as tahun_ajaran_id',
                 'tahun_ajaran.semester',
                 'tahun_ajaran.tahun',
-                DB::raw('GROUP_CONCAT(DISTINCT kelas.nama ORDER BY kelas.nama ASC SEPARATOR ", ") as nama_kelas'),
-                DB::raw('GROUP_CONCAT(DISTINCT users.name ORDER BY users.name ASC SEPARATOR ", ") as nama_wali_kelas')
+                'kelas.nama as nama_kelas',
+                'users.name as nama_wali_kelas',
+                'tahun_ajaran.id as tahun_ajaran_id'
             )
-            ->groupBy('tahun_ajaran.id', 'tahun_ajaran.semester', 'tahun_ajaran.tahun')
-            ->orderBy('tahun_ajaran.id')
-            ->get();
+            ->orderBy('kelas.nama')
+            ->get()->toArray();
 
-        $processedData = $data->map(function ($item) {
-            $item->nama_kelas = explode(', ', $item->nama_kelas);
-            $item->nama_wali_kelas = explode(', ', $item->nama_wali_kelas);
-            return $item;
-        });
+        $formattedData = $this->formatData($data);
+
+        return view('template-laporan-riwayat-wali', ['data' => $formattedData]);
+    }
+
+    private function formatData($data)
+    {
+        $result = array_reduce($data, function ($carry, $item) {
+            $tahunAjaranId = $item->tahun_ajaran_id;
+
+            if (!isset($carry[$tahunAjaranId])) {
+                $carry[$tahunAjaranId] = [
+                    'semester' => $item->semester,
+                    'tahun' => $item->tahun,
+                    'tahun_ajaran_id' => $tahunAjaranId,
+                    'data_kelas' => []
+                ];
+            }
+
+            $carry[$tahunAjaranId]['data_kelas'][] = [
+                'nama_kelas' => $item->nama_kelas,
+                'nama_wali_kelas' => $item->nama_wali_kelas
+            ];
+
+            return $carry;
+        }, []);
+
+        return array_values($result);
     }
 }
