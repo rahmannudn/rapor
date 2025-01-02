c3e5182f998d4ac56e6cd5fe2074ca038612feb9
rahmanuddin <rahmannudn0@gmail.com>
2025-01-03 02:47:21 +0800

    menampilkan tabel subproyek pada detail proyek

--- app/Livewire/Proyek/Detail.php
+++ app/Livewire/Proyek/Detail.php
@@ -20,11 +20,32 @@ class Detail extends Component
             ->first()
             ->toArray();
 
+        $subproyek = $this->getSubProyekInfo();
+
         return view('livewire.proyek.detail', [
             'nama_kelas' => $dataWali['nama_kelas'],
             'nama_wali_kelas' => $dataWali['nama_wali_kelas'],
             'tahun' => $dataWali['tahun'],
             'semester' => $dataWali['semester'],
+            'subproyek' => $subproyek,
         ]);
+    }
+
+    private function getSubProyekInfo()
+    {
+        return Proyek::where('proyek.id', $this->proyek['id'])
+            ->join('subproyek', 'subproyek.proyek_id', 'proyek.id')
+            ->join('capaian_fase', 'capaian_fase.id', 'subproyek.capaian_fase_id')
+            ->join('subelemen', 'subelemen.id', 'capaian_fase.subelemen_id')
+            ->join('elemen', 'elemen.id', 'subelemen.elemen_id')
+            ->join('dimensi', 'dimensi.id', 'elemen.dimensi_id')
+            ->select(
+                'subproyek.id',
+                'capaian_fase.deskripsi as capaian_fase_deskripsi',
+                'subelemen.deskripsi as subelemen_deskripsi',
+                'elemen.deskripsi as elemen_deskripsi',
+                'dimensi.deskripsi as dimensi_deskripsi',
+            )
+            ->get();
     }
 }
--- resources/views/livewire/proyek/detail.blade.php
+++ resources/views/livewire/proyek/detail.blade.php
@@ -1,9 +1,12 @@
 <div>
+    @section('table-responsive')
+        <link rel="stylesheet" href="{{ asset('resources/css/responsive-table.css') }}">
+    @endsection
     <x-slot:title>
         Detail Proyek
     </x-slot:title>
 
-    <x-button href="{{ redirect()->back() }}" wire:navigate class="mb-1" icon="arrow-left" info label="Kembali" />
+    <x-button href="{{ url()->previous() }}" wire:navigate class="mb-1" icon="arrow-left" info label="Kembali" />
 
     {{-- proyek info --}}
     <div class="relative overflow-x-auto w-[55%] mb-4 mt-2">
@@ -57,6 +60,53 @@
         </table>
     </div>
     {{-- proyek info --}}
+
+    {{-- subproyek table --}}
+    <h2 class="mb-4 text-2xl font-bold">Subproyek</h2>
+
+    <div class="container">
+        <table class="w-full responsive-table">
+            <thead class="head-style">
+                <tr>
+                    <th class="cell-border black-text sticky-cell">NO</th>
+                    <th class="cell-border black-text sticky-cell">Dimensi</th>
+                    <th class="cell-border black-text ">Subelemen</th>
+                    <th class="cell-border black-text ">Elemen</th>
+                    <th class="cell-border black-text ">Capaian Fase</th>
+                </tr>
+            </thead>
+            <tbody class="w-full">
+                @forelse($subproyek as $item)
+                    <tr key="{{ $loop->index }}" class="h-20 text-center">
+                        <td class="px-1  w-[5%] text-center cell-border sticky-cell">
+                            {{ $loop->index + 1 }}
+                        </td>
+                        <td scope="row" class="px-6 font-medium cell-border sticky-cell">
+                            {{ $item['dimensi_deskripsi'] }}
+                        </td>
+                        <td scope="row" class="px-6 font-medium cell-border ">
+                            {{ $item['elemen_deskripsi'] }}
+                        </td>
+                        <td scope="row" class="px-6 font-medium cell-border ">
+                            {{ $item['subelemen_deskripsi'] }}
+                        </td>
+                        <td scope="row" class="px-6 font-medium cell-border ">
+                            {{ $item['capaian_fase_deskripsi'] }}
+                        </td>
+                    </tr>
+                @empty
+                    <tr
+                        class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600 cell-border">
+                        <th scope="row"
+                            class="block px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                            Data Tidak Ditemukan
+                        </th>
+                    </tr>
+                @endforelse
+            </tbody>
+        </table>
+    </div>
+    {{-- subproyek table --}}
 
 
 </div>
