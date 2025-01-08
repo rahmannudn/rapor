bc5f538ee0d3c0b9df70305bcd85632db3ba5697
rahmanuddin <rahmannudn0@gmail.com>
2025-01-08 21:02:10 +0800

    membuat layout laporan

---
+++ resources/views/components/laporan-layout/layout.blade.php
@@ -0,0 +1,138 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title')</title>
+    <style>
+        /* General Styles */
+        body {
+            margin: 0;
+            font-family: Arial, sans-serif;
+        }
+
+        h1 {
+            font-size: 23px;
+            text-align: center;
+            margin-bottom: 30px;
+            margin-top: 0;
+        }
+
+        .text-center {
+            text-align: center;
+        }
+
+        thead th {
+            background-color: #f2f2f2;
+            padding: 12px 8px;
+            /* Jarak dalam heading */
+        }
+
+        thead {
+            margin-bottom: 10px;
+            /* Tambahkan jarak di bawah heading */
+        }
+
+        .container {
+            max-width: 21cm;
+            margin: 0 auto;
+            padding: 40px;
+        }
+
+        table {
+            width: 100%;
+            border-collapse: collapse;
+        }
+
+        th,
+        td {
+            border: 1px solid black;
+            padding: 8px;
+            text-align: left;
+        }
+
+        th {
+            background-color: #f2f2f2;
+        }
+
+        /* Hindari putusnya baris di tengah */
+        tr {
+            page-break-inside: avoid;
+        }
+
+        thead th {
+            text-align: center;
+        }
+
+        /* Print button styles */
+        #printButton {
+            background-color: #ff0000;
+            color: white;
+            padding: 20px;
+            border: none;
+            cursor: pointer;
+            /* margin-bottom: 20px; */
+        }
+
+        @media print {
+            #printButton {
+                display: none;
+            }
+
+            body {
+                /* padding: 40px; */
+            }
+
+            .container {
+                /* padding: 1.5cm; */
+            }
+
+            thead {
+                display: table-header-group;
+            }
+
+            /* Reset margin for repeated headers */
+            thead::after {
+                margin-top: 0 !important;
+            }
+
+            /* Control page breaks */
+            tr {
+                page-break-inside: avoid;
+            }
+
+            /* Adjust spacing for subsequent pages */
+            .table-wrapper {
+                margin-top: 0;
+            }
+
+            /* Remove extra space after table on page breaks */
+            table {
+                margin-bottom: 0;
+            }
+        }
+    </style>
+</head>
+
+<body>
+    <div class="container">
+        <button id="printButton">Print</button>
+        @include('components.laporan-layout.header')
+        <h1>@yield('title')</h1>
+
+        @yield('content')
+    </div>
+
+    <script>
+        document.getElementById("printButton").onclick = function() {
+            window.print();
+        }
+        window.onload = function() {
+            window.print();
+        };
+    </script>
+</body>
+
+</html>
