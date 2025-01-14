c0f228079e7c47628077c7246300a5228638024c
rahmanuddin <rahmannudn0@gmail.com>
2025-01-14 13:54:09 +0800

    memperbaiki layout app

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -11,8 +11,8 @@
     </title>
 
     <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    {{-- <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" /> --}}
     @stack('table-responsive')
     <!-- Scripts -->
     @livewireStyles
