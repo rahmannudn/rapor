295c5bd2a57ecf591391bc2486de9feb65823a4f
rahmanuddin <rahmannudn0@gmail.com>
2025-01-14 13:54:55 +0800

    menambahkan layout untuk guest

---
+++ resources/views/components/layouts/guest.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>{{ $title ?? 'Aplikasi Rapor' }}</title>
+    @livewireStyles
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body class="text-gray-800 bg-blue-50">
+    {{ $slot }}
+    <!-- Footer -->
+    <footer class="py-6 text-center text-white bg-blue-500">
+        <p class="text-sm">&copy; 2025 Aplikasi Rapor. Dikembangkan untuk kemudahan staf sekolah dan orang tua siswa.
+        </p>
+    </footer>
+
+    @livewireScripts
+</body>
+
+</html>
